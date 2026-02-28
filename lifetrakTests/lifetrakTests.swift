56183b46ed8cf395b0d1a14c6352bff8080cf476
Dan Foygel <foygel@gmail.com>
2026-02-28 15:27:02 -0800

    Initial Commit

---
+++ lifetrakTests/lifetrakTests.swift
@@ -0,0 +1,17 @@
+//
+//  lifetrakTests.swift
+//  lifetrakTests
+//
+//  Created by Dan Foygel on 2/28/26.
+//
+
+import Testing
+@testable import lifetrak
+
+struct lifetrakTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
